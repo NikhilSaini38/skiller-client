fdc94b1bf816f39d06131b9250e4c2cbebc1d7a9
Nikhil Saini <nkhlsn38@gmail.com>
2018-09-25 21:21:48 +1000

    wireframe data and colors added

--- app-root-navigator/index.js
+++ app-root-navigator/index.js
@@ -1,11 +1,13 @@
 import {createSwitchNavigator,createStackNavigator} from 'react-navigation';
 //@index(F:/.js):import ${variable:pascal} from ${relpath}
+import Login from "./login"
 import SplashScreen from "./splash-screen"
 ///index
 
 export default createSwitchNavigator(
     {
         //@index(F:/.js):"${variable:kebab}":${variable:pascal},
+        "login":Login,
         "splash-screen":SplashScreen,
         ///index
     }
---
+++ app-root-navigator/login/index.js
@@ -0,0 +1,15 @@
+import Styles from "./login.styles.js";
+import Functions from "./login.functions.js";
+import Component from "./login.component.js";
+
+//@index(F: \.js$,F: !\.styles.js, F: !\.component.js, F: !\.functions.js): import ${variable:pascal} from ${relpath};
+///index
+
+export {
+    Styles,
+    Functions,
+    //@index(F: \.js$,F: !\.styles.js, F: !\.component.js, F: !\.functions.js): ${variable:pascal},
+    ///index
+};
+
+export default Component;
